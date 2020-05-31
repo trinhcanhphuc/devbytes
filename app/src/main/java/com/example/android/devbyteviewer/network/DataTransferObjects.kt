412f5786a3003496697c0886a74e673103337b6e
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-05-31 10:04:36 +0700

    Make offline cache with repository

--- app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.devbyteviewer.network
 
+import com.example.android.devbyteviewer.database.DatabaseVideo
 import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.squareup.moshi.JsonClass
 
@@ -54,9 +55,9 @@ data class NetworkVideo(
 /**
  * Convert Network results to database objects
  */
-fun NetworkVideoContainer.asDomainModel(): List<DevByteVideo> {
+fun NetworkVideoContainer.asDatabaseModel(): List<DatabaseVideo> {
     return videos.map {
-        DevByteVideo(
+        DatabaseVideo(
                 title = it.title,
                 description = it.description,
                 url = it.url,
--- app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
+++ app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
@@ -22,9 +22,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.android.devbyteviewer.domain.DevByteVideo
-import com.example.android.devbyteviewer.network.DevByteNetwork
-import com.example.android.devbyteviewer.network.asDomainModel
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
 import kotlinx.coroutines.*
 import java.io.IOException
 
@@ -55,20 +54,9 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
      */
     private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
 
-    /**
-     * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
-     * way to set this value to observers.
-     */
-    private val _playlist = MutableLiveData<List<DevByteVideo>>()
+    private val videosRepository = VideosRepository(getDatabase(application))
 
-    /**
-     * A playlist of videos that can be shown on the screen. Views should use this to get access
-     * to the data.
-     */
-    val playlist: LiveData<List<DevByteVideo>>
-        get() = _playlist
-
-
+    val playlist = videosRepository.videos
 
     /**
      * Event triggered for network error. This is private to avoid exposing a
@@ -100,25 +88,21 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
      * init{} is called immediately when this ViewModel is created.
      */
     init {
-        refreshDataFromNetwork()
+        refreshDataFromRepository()
     }
 
     /**
      * Refresh data from network and pass it via LiveData. Use a coroutine launch to get to
      * background thread.
      */
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
-
+    private fun refreshDataFromRepository() = viewModelScope.launch {
         try {
-             val playlist = DevByteNetwork.devbytes.getPlaylist().await()
-            _playlist.postValue(playlist.asDomainModel())
-
+            videosRepository.refreshVideos()
             _eventNetworkError.value = false
             _isNetworkErrorShown.value = false
-
         } catch (networkError: IOException) {
-            // Show a Toast error message and hide the progress bar.
-            _eventNetworkError.value = true
+            if(playlist.value.isNullOrEmpty())
+                _eventNetworkError.value = true
         }
     }
 
